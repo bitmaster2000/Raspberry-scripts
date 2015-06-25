791544b7662995ffb2fadeee8bcfc2e61d6d02f5
bitmaster2000 <bitmaster2000@gmail.com>
2015-06-25 15:04:26 +0200

    Simplified license

--- install-webserver.sh
+++ install-webserver.sh
@@ -11,13 +11,8 @@
 # work on other Debian flavors too.
 #
 # This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 2 of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# it under the terms of the MIT License. See the LICENSE file for more
+# information.
 #
 
 function e {
